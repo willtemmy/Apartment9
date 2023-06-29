a7855517fc2f5b35c9fee2ace44dd2b8e1bb07df
regina <reggiekariuki58@gmail.com>
2023-06-29 11:40:51 +0300

    index and show

--- app/controllers/apartmentts_controller.rb
+++ app/controllers/apartmentts_controller.rb
@@ -1,2 +1,14 @@
 class ApartmenttsController < ApplicationController
+    def index 
+        apartment = Apartmentt.all 
+        render json:apartment
+    end
+
+    def show
+        apartment = Apartmentt.find_by(id:params[:id])
+        render json:apartment
+    end
 end
+
+
+    
